54e8b7fc5102278aadaffaca5bb73955755f172f
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-11 12:45:50 +0530

    test: generation of image

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Dynamically Generate OG Images in your Frappe site.
 
 ## Planned Features
 
+- [ ] Delete og image files on trash of document
 - [ ] Configurable fonts via Google Fonts
 - [ ] Update existing OG images on documents in bulk from OG Template form/API
 - [ ] Use something like [this](https://github.com/jonkemp/inline-css) to support style tags instead of manual inline CSS
---
+++ frappe_dynamic_og/tests/test_image_generation.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2022, Hussain Nagaria and Contributors
+# See license.txt
+
+import frappe
+
+from frappe.tests.utils import FrappeTestCase
+from frappe.core.api.file import get_attached_images
+
+class TestImageGeneration(FrappeTestCase):
+	def setUp(self):
+		# Disable any existing template
+		frappe.db.set_value("OG Image Template", {"is_enabled": True}, "is_enabled", False)
+
+		# Create new test template
+		self.test_og_template = frappe.get_doc(
+			{
+				"doctype": "OG Image Template",
+				"for_doctype": "ToDo",
+				"is_enabled": False,  # enable before using,
+				"template_html": '<div style="width: 800px; background-color: #ff0000; display: flex;" ><h1>{{ doc.description }}</h1></div>',
+			}
+		)
+
+	def test_does_not_generate_if_template_disabled(self):
+		test_todo_doc = frappe.get_doc({
+			"doctype": "ToDo",
+			"description": "Hello, Hussain!"
+		}).insert()
+
+		attached_images = get_attached_images("ToDo", [test_todo_doc.name])
+		self.assertEqual(len(attached_images), 0)
+
+	def test_image_attached_if_template_enabled(self):
+		# enable the test template
+		self.test_og_template.is_enabled = True
+		self.test_og_template.save()
+
+		# create a new todo doc
+		test_todo_doc = frappe.get_doc({
+			"doctype": "ToDo",
+			"description": "Bro, what's up?"
+		}).insert()
+
+		attached_images = get_attached_images("ToDo", [test_todo_doc.name])
+		self.assertEqual(len(attached_images), 1)
+		self.assertTrue(attached_images[test_todo_doc.name][0].startswith("/files/og_image_todo_"))
