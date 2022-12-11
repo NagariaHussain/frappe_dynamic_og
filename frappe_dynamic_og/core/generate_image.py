c124f03fafe06541b476e360b9481c25a6333b36
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-11 10:37:43 +0530

    feat: delete older image files when a new one is generated

--- frappe_dynamic_og/core/generate_image.py
+++ frappe_dynamic_og/core/generate_image.py
@@ -14,11 +14,24 @@ def generate_and_attach_og_image(doc, method=None):
     ):
         return
 
+    file_name = get_file_name(doc)
     content = get_processed_html_content(doc)
     stdout, stderr = generate_and_get_image_from_node_process(content)
 
     if not stderr:
-        create_file_doc(get_file_name(doc), stdout, doc.doctype, doc.name)
+        file_doc = create_file_doc(file_name, stdout, doc.doctype, doc.name)
+    else:
+        # Maybe add a comment to the document?
+        # + log an error?
+        frappe.throw("Error generating image file")
+
+    try:
+        delete_old_images_if_applicable(doc, file_doc.name)
+    except Exception:
+        doc.log_error(
+            "Error Deleting Old OG Images",
+            "There was an error while deleting old OG images",
+        )
 
 
 def create_file_doc(name, content, attached_to_doctype, attached_to_name):
@@ -28,6 +41,8 @@ def create_file_doc(name, content, attached_to_doctype, attached_to_name):
     file_doc.attached_to_doctype = attached_to_doctype
     file_doc.attached_to_name = attached_to_name
     file_doc.save()
+
+    return file_doc
 
 
 def get_file_name(doc):
@@ -57,3 +72,20 @@ def generate_and_get_image_from_node_process(html_content):
     )
 
     return process.communicate()
+
+
+def delete_old_images_if_applicable(doc, new_file_doc_name):
+    to_delete = frappe.db.get_single_value(
+        "Frappe Dynamic OG Settings", "automatically_delete_old_images"
+    )
+
+    if to_delete:
+        doc_info = frappe.scrub(f"{doc.doctype}_{doc.name}")
+        old_image_files = frappe.db.get_all(
+            "File", {"file_name": ("like", f"og_image_{doc_info}%")}, pluck="name"
+        )
+        for name in old_image_files:
+            if name == new_file_doc_name:
+                # this is the newly created file!
+                continue
+            frappe.delete_doc("File", name)
