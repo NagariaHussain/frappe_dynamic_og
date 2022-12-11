baddbb11cf1e8432d37af202dfd3f85ebc9376fd
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-11 11:43:36 +0530

    feat: validate more than 1 enabled templates at a time

--- frappe_dynamic_og/frappe_dynamic_og/doctype/og_image_template/og_image_template.py
+++ frappe_dynamic_og/frappe_dynamic_og/doctype/og_image_template/og_image_template.py
@@ -1,8 +1,34 @@
 # Copyright (c) 2022, Hussain Nagaria and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-class OGImageTemplate(Document):
+
+class EnabledTemplateAlreadyExistsException(Exception):
+	"""An enabled OG Image Template Already exists"""
+
 	pass
+
+
+class OGImageTemplate(Document):
+	def before_save(self):
+		if self.has_value_changed("is_enabled"):
+			self.validate_if_enabled_already_exists()
+
+	def validate_if_enabled_already_exists(self):
+		if self.is_enabled:
+			enabled_exists = frappe.db.exists(
+				"OG Image Template",
+				{
+					"for_doctype": self.for_doctype,
+					"is_enabled": True,
+					"name": ("!=", self.name),
+				},
+			)
+
+			if enabled_exists:
+				frappe.throw(
+					"A template is already enabled for this doctype",
+					EnabledTemplateAlreadyExistsException,
+				)
