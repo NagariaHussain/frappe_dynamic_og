92f6f3427f69e5c960697fdf4e31e7b82875780c
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-12 08:04:24 +0530

    feat: field to generate/view preview image

--- frappe_dynamic_og/frappe_dynamic_og/doctype/og_image_template/og_image_template.js
+++ frappe_dynamic_og/frappe_dynamic_og/doctype/og_image_template/og_image_template.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2022, Hussain Nagaria and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("OG Image Template", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("OG Image Template", {
+  refresh(frm) {},
+  template_html(frm) {
+    // runs on change
+    // console.log("template_html changed.")
+  },
+  generate_preview_button(frm) {
+    console.log("button clicked.");
+  },
+});
