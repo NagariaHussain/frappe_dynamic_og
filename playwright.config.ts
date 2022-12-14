8380f8155bf5adf2bcd63ad795e3e745825a5913
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-14 23:56:40 +0530

    chore: no webkit project

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,13 +58,6 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
