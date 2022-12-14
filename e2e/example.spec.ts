1dbff098e18b553572f4f975291da3e777a3adda
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-15 00:16:51 +0530

    fix: test site URL in example spec

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("http://test_site.localhost:8000/login");
+  await page.goto("http://f-dog.test:8000/login");
   const loginPageHeading = await page.getByRole("heading", {
     name: "Login to Frappe",
   });
