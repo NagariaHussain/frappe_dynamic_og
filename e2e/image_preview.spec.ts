343161561ae69d68aa0c75a32f5cdd48e38eef51
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-23 23:43:46 +0530

    fix: ui test uri

--- e2e/image_preview.spec.ts
+++ e2e/image_preview.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("http://abc.localhost:8000/");
+  await page.goto("http://f-dog.test:8000/login");
   await page.getByPlaceholder("jane@example.com").fill("Administrator");
   await page.getByPlaceholder("jane@example.com").press("Tab");
   await page.getByPlaceholder("•••••").fill("admin");
